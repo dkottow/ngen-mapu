b336ad4dc4b18d498fb7d314e57ee6f634148a70
Daniel <dkottow@gmail.com>
2015-12-01 21:20:36 +0000

    asome random libs to assist in data generation

--- gen_data/create-sandwich-data.js
+++ gen_data/create-sandwich-data.js
@@ -3,22 +3,25 @@ var assert = require('assert')
 	, _ = require('underscore')
 	, util = require('util')
 	, sqlite3 = require('sqlite3').verbose()
-	, math = require('math')
+	, probdist = require('probdist')
+	, Random = require('random-js')
 	, schema = require('../app/Schema')
 	, database = require('../app/Database');
 	
 var log = global.log;
 
-//run me from root dir mocha etc/create-sandwich-data.js to upload data to sales_sandwich.sqlite
+//run me from root dir mocha gen_data/create-sandwich-data.js to upload data to sales_sandwich.sqlite
 
 describe('Database', function() {
 
 
 	describe('create_orders()', function() {
-		var dbFile = "etc/sales_sandwich.sqlite";
+		var dbFile = "gen_data/sales_sandwich.sqlite";
 		var db = new database.Database(dbFile);
 		var customers;
 		var products;
+		
+		var rand =  new Random(Random.engines.mt19937().autoSeed());
 		
 		before(function(done) {
 			db.init(function(err) {
@@ -43,7 +46,12 @@ describe('Database', function() {
 		});	
 
 		it('create example', function(done) {
-			log.debug(products);
+			var startDate = new Date('2014-01-01');
+			var endDate = new Date('2015-12-01');
+			_.times(50, function() {
+				console.log(rand.date(startDate, endDate));
+			});
+			//log.debug(products);
 			done();
 		});
 	});

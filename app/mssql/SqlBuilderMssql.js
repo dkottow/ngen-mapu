401c6dbe38390382e73958a7c825be1b36b0acc2
Daniel <dkottow@gmail.com>
2017-04-26 22:57:50 +0000

    adding forgotten files..

---
+++ app/mssql/SqlBuilderMssql.js
@@ -0,0 +1,61 @@
+/*
+   Copyright 2016 Daniel Kottow
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+var _ = require('underscore');
+var util = require('util');
+
+var Field = require('../Field.js').Field;
+var Table = require('../Table.js').Table;
+var Schema = require('../Schema.js').Schema;
+
+var SqlHelper = require('./SqlHelperMssql.js').SqlHelperMssql;
+var SqlBuilder = require('../SqlBuilder.js').SqlBuilder;
+
+var log = require('../log.js').log;
+
+var SqlBuilderMssql = function(tableGraph) {
+	SqlBuilder.call(this, tableGraph);
+}
+
+SqlBuilderMssql.prototype = Object.create(SqlBuilder.prototype);	
+
+
+SqlBuilderMssql.prototype.joinSearchSQL = function(filterClauses) {
+	//there can be only one search filter
+	var searchFilter = _.find(filterClauses, function(filter) {
+		return filter.op == 'search' && filter.field == Table.ALL_FIELDS;
+	});
+
+	if (searchFilter) {
+		var table = this.graph.table(searchFilter.table);
+		var clause = util.format('(%s.docid = %s.id)',
+			table.ftsName(),
+			table.name);
+
+		var result = {
+			tables: [],
+			clauses: []
+		};
+		
+		return result;
+
+	} else {
+		return { tables: [], clauses: [] };
+	}
+}
+
+exports.SqlBuilderMssql = SqlBuilderMssql;
+
---
+++ app/mssql/SqlHelperMssql.js
@@ -0,0 +1,106 @@
+/*
+   Copyright 2016 Daniel Kottow
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+var _ = require('underscore');
+var util = require('util');
+var assert = require('assert');
+
+var log = require('../log.js').log;
+
+var SqlHelperMssql = {
+	Field: {},
+	Table: {},
+	Schema: {}
+}
+
+
+SqlHelperMssql.EncloseSQL = function(name) {
+	return '[' + name + ']';
+}
+
+SqlHelperMssql.ConcatSQL = function(values) {
+	return values.join(' + '); 
+}
+
+
+/********** Schema stuff *********/
+
+SqlHelperMssql.Schema.PragmaSQL = '';
+
+SqlHelperMssql.Schema.createPropsTableSQL = function(name) {
+	return "CREATE TABLE " + name + " ("
+		+ " name VARCHAR(256) NOT NULL, "
+		+ "	value VARCHAR(MAX), "
+		+ "	PRIMARY KEY (name) "
+		+ ");\n\n"
+}
+
+/******** Table stuff ********/
+
+SqlHelperMssql.Table.createPropsTableSQL = function(name) {
+	return "CREATE TABLE " + name + " ("
+		+ " name VARCHAR(256) NOT NULL, "
+		+ "	props VARCHAR(MAX), "
+		+ " disabled INTEGER DEFAULT 0, "
+		+ "	PRIMARY KEY (name) "
+		+ ");\n\n";
+}
+
+
+/*** TODO see here
+https://docs.microsoft.com/en-us/sql/t-sql/statements/create-fulltext-index-transact-sql
+****/
+
+SqlHelperMssql.Table.createSearchSQL = function(table) {
+	var sql = '';
+	log.trace({ sql: sql }, 'Table.createSearchSQL()');
+	return sql;
+}
+
+/********** Field **********/
+
+SqlHelperMssql.Field.createPropsTableSQL = function(name) {
+	return " CREATE TABLE " + name + " ("
+		+ ' table_name VARCHAR(256) NOT NULL, '
+		+ ' name VARCHAR(256) NOT NULL, '
+		+ ' props VARCHAR(MAX), '
+		+ ' disabled INTEGER DEFAULT 0, '
+		+ ' PRIMARY KEY (name, table_name) '
+		+ ");\n\n";
+}
+		
+SqlHelperMssql.Field.defaultSQL = function(field) {
+
+	if (_.contains(['mod_on', 'add_on'], field.name)) {
+		return "DEFAULT GETDATE()";
+
+	} else if (_.contains(['mod_by', 'add_by'], field.name)) {
+		return "DEFAULT 'sql'";
+
+	} else {
+		return '';
+	}
+}
+
+SqlHelperMssql.Field.typeSQL = function(type)
+{
+	if (type == 'VARCHAR') return 'VARCHAR(MAX)';
+	return type;
+}
+
+
+exports.SqlHelperMssql = SqlHelperMssql;
+

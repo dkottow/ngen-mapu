6a5ccac0824c4571901b6ad4635f930d324d135c
Kottow <dkottow@golder.com>
2018-02-16 11:46:26 -0300

    tweaked http headers to allow SP auth method based on cookies (exp).

--- app/app.js
+++ app/app.js
@@ -66,11 +66,12 @@ function initRoutes(options) {
 
 	//enable CORS
 	app.use(function(req, res, next) {
-	  res.header("Access-Control-Allow-Origin", "*");
-	  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH, OPTIONS'); 
-	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers");
-	  if (req.method == 'OPTIONS') res.status(200).send();
-	  else next();
+		res.header("Access-Control-Allow-Credentials", "true");
+		res.header("Access-Control-Allow-Origin", config.http.headers.AccessControlAllowOrigin);
+		res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH, OPTIONS'); 
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers");
+		if (req.method == 'OPTIONS') res.status(200).send();
+		else next();
 	});
 
 	//signup
